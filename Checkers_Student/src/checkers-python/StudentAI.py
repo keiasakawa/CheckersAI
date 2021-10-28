788e61322f21b012b079a1b0e1eacc1925d3a608
lbdoan-9999 <lbdoan@uci.edu>
2021-10-27 23:44:27 -0700

    Provide test script @Tools for N*x system. Provide function shells and working timer.

--- Checkers_Student/src/checkers-python/StudentAI.py
+++ Checkers_Student/src/checkers-python/StudentAI.py
@@ -1,51 +1,93 @@
-from random import randint
-from timeit import default_timer
-from BoardClasses import Move
 from BoardClasses import Board
+from BoardClasses import Move
+from datetime     import timedelta
+from random       import randint
+from timeit       import default_timer as timer
+
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
+
 class StudentAI():
 
-    total_time = 0.0
-    TIME_OF_TIMEOUT = 480
+    # Hard coded time limit in second(s).
+    # TODO: Change it 480 for submission as 480 is way too long for test runs.
+    
+    TIME_LIMIT = 1
 
-    def __init__(self,col,row,p):
+    def __init__(self, col, row, p):
+        
         self.col = col
         self.row = row
         self.p = p
-        self.board = Board(col,row,p)
+        self.board = Board(col, row, p)
         self.board.initialize_game()
         self.color = ''
-        self.opponent = {1:2,2:1}
+        self.opponent = {1:2, 2:1}
         self.color = 2
     
-    def get_move(self,move):
-        timeStart = default.timer() # keep track of time
+    def get_move(self, move):
+        
+        # Keep track of time.
+        timeStart = timer()
+        
         if len(move) != 0:
-            self.board.make_move(move,self.opponent[self.color])
+            self.board.make_move(move, self.opponent[self.color])
         else:
             self.color = 1
-        moves = self.board.get_all_possible_moves(self.color)
-
-            index = randint(0,len(moves)-1)
-            inner_index =  randint(0,len(moves[index])-1)
-            move = moves[index][inner_index]
-        # if it's only one move then return it
-        if len(moves) == 1:
-            timeEnd = 
-            return moves[0]
-        while remainingTime >= 3
-            # index = randint(0,len(moves)-1)
-            # inner_index =  randint(0,len(moves[index])-1)
-            # move = moves[index][inner_index]
+        
+        moves       = self.board.get_all_possible_moves(self.color)
+        index       = randint(0, len(moves) - 1)        # TODO: Remove later.
+        inner_index = randint(0, len(moves[index]) - 1) # TODO: Remove later.
+        move        = moves[index][inner_index]         # TODO: Remove later.
+        
+        # If there's only one move, then return it.
+        
+        # if len(moves) == 1:  # Doesn't work as moves is a list of move
+        #     return moves[0]  # move objects, not moves themself.
+         
+        while timer() < timeStart + self.TIME_LIMIT:
+            
+            # TODO: Should call MCTS here.
+            
+            pass
 
         self.board.make_move(move,self.color)
         return move
 
-    # Monte Carlo Tree Search
+# ==== MONTE-CARLO TREE SEARCH =============================================== #
+    
+    # TODO: REQUIRED
+    def evaluate():
+    
+    	""" Evaluation function to decide whether to explore or exploit. """
+    	
+    	pass
+    
+    # TODO: Optional
+    def load_tree():
+    	
+    	""" Load game tree of previous game(s). """
+    	
+    	pass
+    
+    
+    # TODO: Optional
+    def log_tree():
+    	
+    	""" Write to game tree to log for future game(s). """
+    	
+    	pass
+    
+    # TODO: REQUIRED
+    # We might have to code MCTS as a class object as it will be easier to
+    # manage the nodes.
     def mcts(state):
-     # we do alpha-beta pruning to get rid of nodes we do not need to explore
-    def alphabeta(state, player,  alpha, beta):
+        
+        pass
+    
+    # we do alpha-beta pruning to get rid of nodes we do not need to explore
+    
+    def alphabeta(state, player, alpha, beta):
         # we check to see if the current state is end
         if state.is_win() == 2:
             return state
